a11e8cc8b0de70813e9b364d48a1f80150816a3a
cykuo <cykuotw45@gmail.com>
2024-05-14 14:29:25 -0400

    feat: add group store UpdateGroupMember

--- services/group/store.go
+++ services/group/store.go
@@ -211,6 +211,25 @@ func (s *Store) GetGroupMemberByGroupID(groupID string) ([]*types.User, error) {
 }
 
 func (s *Store) UpdateGroupMember(action string, userID string, groupID string) error {
+	query := ""
+	if action == "add" {
+		query = fmt.Sprintf(
+			"INSERT INTO group_member ("+
+				"id, group_id, user_id"+
+				") VALUES ('%s', '%s', '%s')",
+			uuid.NewString(), groupID, userID,
+		)
+	} else if action == "delete" {
+		query = fmt.Sprintf(
+			"DELETE FROM group_member WHERE group_id='%s' AND user_id='%s';",
+			groupID, userID,
+		)
+	}
+	_, err := s.db.Exec(query)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 

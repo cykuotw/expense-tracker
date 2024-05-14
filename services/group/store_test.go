56f4110a1012ad6330d9cee2224fbd8bd027b24e
cykuo <cykuotw45@gmail.com>
2024-05-13 20:31:04 -0400

    test: add group test createGroup

---
+++ services/group/store_test.go
@@ -0,0 +1,82 @@
+package group_test
+
+import (
+	"database/sql"
+	"expense-tracker/config"
+	"expense-tracker/db"
+	"expense-tracker/services/group"
+	"expense-tracker/types"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateGroup(t *testing.T) {
+	// prepare test data
+	cfg := config.Envs
+	db, _ := db.NewPostgreSQLStorage(cfg)
+	store := group.NewStore(db)
+
+	// define test cases
+	type testcase struct {
+		name        string
+		mockGroup   types.Group
+		expectFail  bool
+		expectError error
+	}
+
+	subtests := []testcase{
+		{
+			name: "valid",
+			mockGroup: types.Group{
+				ID:           uuid.New(),
+				GroupName:    "test",
+				Description:  "test desc",
+				CreateTime:   time.Now(),
+				IsActive:     true,
+				CreateByUser: uuid.New(),
+			},
+			expectFail:  false,
+			expectError: nil,
+		},
+	}
+
+	for _, test := range subtests {
+		t.Run(test.name, func(t *testing.T) {
+			err := store.CreateGroup(test.mockGroup)
+			defer deleteGroup(db, test.mockGroup.ID)
+
+			assert.Equal(t, test.expectError, err)
+		})
+	}
+}
+
+func insertGroup(db *sql.DB, group types.Group) {
+	createTime := group.CreateTime.UTC().Format("2006-01-02 15:04:05-0700")
+	statement := fmt.Sprintf(
+		"INSERT INTO groups ("+
+			"id, group_name, description, "+
+			"create_time_utc, is_active, create_by_user_id"+
+			") VALUES ('%s', '%s', '%s', '%s', '%t', '%s');",
+		group.ID, group.GroupName, group.Description,
+		createTime, group.IsActive, group.CreateByUser,
+	)
+
+	db.Exec(statement)
+}
+
+func deleteGroup(db *sql.DB, groupId uuid.UUID) {
+	statement := fmt.Sprintf("DELETE FROM groups WHERE id='%s';", groupId)
+	db.Exec(statement)
+}
+
+func insertGroupMember(db *sql.DB, groupId uuid.UUID, members []types.User) {
+
+}
+
+func deleteGroupMember(db *sql.DB, groupId uuid.UUID, userids []uuid.UUID) {
+
+}

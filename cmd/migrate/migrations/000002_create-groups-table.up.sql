ffae4c825abdf912a739102f1472ca727ada781c
cykuo <cykuotw45@gmail.com>
2024-05-13 20:03:46 -0400

    fix: db table groups group_name type

--- cmd/migrate/migrations/000002_create-groups-table.up.sql
+++ cmd/migrate/migrations/000002_create-groups-table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS groups(
     "id" UUID NOT NULL PRIMARY KEY,
-    "group_name" UUID NOT NULL,
+    "group_name" VARCHAR(32) NOT NULL,
     "description" VARCHAR(256),
     "create_time_utc" TIMESTAMP WITH TIME ZONE NOT NULL,
     "is_active" BOOLEAN NOT NULL,

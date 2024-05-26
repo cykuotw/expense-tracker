e70d6c11a69886fb72ef91f43c3f8c34e2f06ead
cykuo <cykuotw45@gmail.com>
2024-05-25 20:03:47 -0400

    feat: add CreateItem function and test in expense store

--- services/expense/store.go
+++ services/expense/store.go
@@ -42,6 +42,20 @@ func (s *Store) CreateExpense(expense types.Expense) error {
 }
 
 func (s *Store) CreateItem(item types.Item) error {
+	query := fmt.Sprintf(
+		"INSERT INTO item ("+
+			"id, expense_id, name, amount, "+
+			"unit, unit_price"+
+			") VALUES ('%s', '%s', '%s', '%s', '%s', '%s');",
+		item.ID, item.ExpenseID, item.Name, item.Amount.String(),
+		item.Unit, item.UnitPrice.String(),
+	)
+
+	_, err := s.db.Exec(query)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
--- services/expense/store_test.go
+++ services/expense/store_test.go
@@ -15,7 +15,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCreateGroup(t *testing.T) {
+func TestCreateExpense(t *testing.T) {
 	// prepare test data
 	cfg := config.Envs
 	db, _ := db.NewPostgreSQLStorage(cfg)
@@ -63,11 +63,56 @@ func TestCreateGroup(t *testing.T) {
 	}
 }
 
+func TestCreateItem(t *testing.T) {
+	// prepare test data
+	cfg := config.Envs
+	db, _ := db.NewPostgreSQLStorage(cfg)
+	store := expense.NewStore(db)
+
+	// define test cases
+	type testcase struct {
+		name        string
+		mockItem    types.Item
+		expectFail  bool
+		expectError error
+	}
+
+	subtests := []testcase{
+		{
+			name: "valid",
+			mockItem: types.Item{
+				ID:        uuid.New(),
+				ExpenseID: uuid.New(),
+				Name:      "test name",
+				Amount:    decimal.NewFromFloat(3.7),
+				Unit:      "ea",
+				UnitPrice: decimal.NewFromFloat(2.9),
+			},
+			expectFail:  false,
+			expectError: nil,
+		},
+	}
+
+	for _, test := range subtests {
+		t.Run(test.name, func(t *testing.T) {
+			err := store.CreateItem(test.mockItem)
+			defer deleteItem(db, test.mockItem.ID)
+
+			assert.Equal(t, test.expectError, err)
+		})
+	}
+}
+
 var mockGroupID = uuid.New()
 var mockCreatorID = uuid.New()
 var mockPayerID = uuid.New()
 
 func deleteExpense(db *sql.DB, expenseId uuid.UUID) {
 	query := fmt.Sprintf("DELETE FROM expense WHERE id='%s';", expenseId)
+	db.Exec(query)
+}
+
+func deleteItem(db *sql.DB, itemID uuid.UUID) {
+	query := fmt.Sprintf("DELETE FROM item WHERE id='%s';", itemID)
 	db.Exec(query)
 }

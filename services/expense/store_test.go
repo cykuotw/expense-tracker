98d22cf1eaff54b49c7e44a7ecc08356515031d8
cykuo <cykuotw45@gmail.com>
2024-05-27 13:56:51 -0400

    test: add test for GetItemsByExpenseID

--- services/expense/store_test.go
+++ services/expense/store_test.go
@@ -304,6 +304,79 @@ func TestGetExpenseList(t *testing.T) {
 	}
 }
 
+func TestGetItemsByExpenseID(t *testing.T) {
+	// prepare test data
+	cfg := config.Envs
+	db, _ := db.NewPostgreSQLStorage(cfg)
+	store := expense.NewStore(db)
+
+	mockExpenseID := uuid.New()
+
+	testSetSize := 13
+	itemIDs := []uuid.UUID{}
+	for i := 0; i < testSetSize; i++ {
+		id := uuid.New()
+		itemIDs = append(itemIDs, id)
+
+		item := types.Item{
+			ID:        id,
+			ExpenseID: mockExpenseID,
+			Name:      "test " + strconv.Itoa(i),
+			Amount:    decimal.NewFromFloat(3.66 + float64(i)),
+			Unit:      "lbs",
+			UnitPrice: decimal.NewFromFloat(0.7 + float64(i)),
+		}
+		insertItem(db, item)
+	}
+	defer deleteItems(db, itemIDs)
+
+	// prepare test case
+	type testcase struct {
+		name         string
+		expenseID    string
+		expectFail   bool
+		expectLength int
+		expectItemID []uuid.UUID
+		expectError  error
+	}
+
+	subtests := []testcase{
+		{
+			name:         "valid",
+			expenseID:    mockExpenseID.String(),
+			expectFail:   false,
+			expectLength: testSetSize,
+			expectItemID: itemIDs,
+			expectError:  nil,
+		},
+		{
+			name:         "invalid expense id",
+			expenseID:    uuid.NewString(),
+			expectFail:   true,
+			expectLength: 0,
+			expectItemID: nil,
+			expectError:  types.ErrExpenseNotExist,
+		},
+	}
+
+	for _, test := range subtests {
+		t.Run(test.name, func(t *testing.T) {
+			itemList, err := store.GetItemsByExpenseID(test.expenseID)
+
+			if test.expectFail {
+				assert.Nil(t, itemList)
+				assert.Equal(t, test.expectError, err)
+			} else {
+				assert.NotNil(t, itemList)
+				assert.Equal(t, test.expectLength, len(itemList))
+				for i := 0; i < test.expectLength; i++ {
+					assert.Contains(t, test.expectItemID, itemList[i].ID)
+				}
+			}
+		})
+	}
+}
+
 var mockGroupID = uuid.New()
 var mockCreatorID = uuid.New()
 var mockPayerID = uuid.New()
@@ -342,9 +415,26 @@ func deleteExpenses(db *sql.DB, expenseIds []uuid.UUID) {
 	}
 }
 
+func insertItem(db *sql.DB, item types.Item) {
+	query := fmt.Sprintf(
+		"INSERT INTO item ("+
+			"id, expense_id, name, amount, unit, unit_price"+
+			") VALUES ('%s', '%s', '%s', '%s', '%s', '%s')",
+		item.ID, item.ExpenseID, item.Name, item.Amount, item.Unit, item.UnitPrice,
+	)
+	db.Exec(query)
+}
+
 func deleteItem(db *sql.DB, itemID uuid.UUID) {
 	query := fmt.Sprintf("DELETE FROM item WHERE id='%s';", itemID)
 	db.Exec(query)
+}
+
+func deleteItems(db *sql.DB, itemIDs []uuid.UUID) {
+	for _, id := range itemIDs {
+		query := fmt.Sprintf("DELETE FROM item WHERE id='%s';", id)
+		db.Exec(query)
+	}
 }
 
 func deleteLedger(db *sql.DB, ledgerID uuid.UUID) {

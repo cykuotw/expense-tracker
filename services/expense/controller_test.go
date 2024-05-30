e74712a3113ac3d23517edfb9e927fbb6ed8d60b
cykuotw <cykuotw45@gmail.com>
2024-05-30 14:19:20 -0400

    test: add test case for DebtSimplify

--- services/expense/controller_test.go
+++ services/expense/controller_test.go
@@ -19,92 +19,152 @@ func TestDebtSimplify(t *testing.T) {
 	DavidID := uuid.New()
 	EmaID := uuid.New()
 
-	ledgers := []*types.Ledger{
-		// Gabe owes Bob $30
+	type testcase struct {
+		name          string
+		ledgers       []*types.Ledger
+		expectFail    bool
+		expectBalance []*types.Balance
+	}
+
+	subtests := []testcase{
 		{
-			LenderUserID:   BobID,
-			BorrowerUesrID: GabeID,
-			Share:          decimal.NewFromInt(30),
+			name: "valid 1",
+			ledgers: []*types.Ledger{
+				// Gabe owes Bob $30
+				{
+					LenderUserID:   BobID,
+					BorrowerUesrID: GabeID,
+					Share:          decimal.NewFromInt(30),
+				},
+				// Gabe owes David $10
+				{
+					LenderUserID:   DavidID,
+					BorrowerUesrID: GabeID,
+					Share:          decimal.NewFromInt(10),
+				},
+				// Fred owes Bob $10
+				{
+					LenderUserID:   BobID,
+					BorrowerUesrID: FredID,
+					Share:          decimal.NewFromInt(10),
+				},
+				// Fred owes Charlie $30
+				{
+					LenderUserID:   CharlieID,
+					BorrowerUesrID: FredID,
+					Share:          decimal.NewFromInt(30),
+				},
+				// Fred owes David $10
+				{
+					LenderUserID:   DavidID,
+					BorrowerUesrID: FredID,
+					Share:          decimal.NewFromInt(10),
+				},
+				// Fred owes Ema $10
+				{
+					LenderUserID:   EmaID,
+					BorrowerUesrID: FredID,
+					Share:          decimal.NewFromInt(10),
+				},
+				// Bob owes Charlie $40
+				{
+					LenderUserID:   CharlieID,
+					BorrowerUesrID: BobID,
+					Share:          decimal.NewFromInt(40),
+				},
+				// Charlie owes David $20
+				{
+					LenderUserID:   DavidID,
+					BorrowerUesrID: CharlieID,
+					Share:          decimal.NewFromInt(20),
+				},
+				// David owes Ema $50
+				{
+					LenderUserID:   EmaID,
+					BorrowerUesrID: DavidID,
+					Share:          decimal.NewFromInt(50),
+				},
+			},
+			expectFail: false,
+			expectBalance: []*types.Balance{
+				// Ema send Fred $60
+				{
+					SenderUserID:   EmaID,
+					ReceiverUserID: FredID,
+					Share:          decimal.NewFromInt(60),
+				},
+				// Charlie send Gabe $40
+				{
+					SenderUserID:   CharlieID,
+					ReceiverUserID: GabeID,
+					Share:          decimal.NewFromInt(40),
+				},
+				// Gabe send David $20
+				{
+					SenderUserID:   CharlieID,
+					ReceiverUserID: DavidID,
+					Share:          decimal.NewFromInt(10),
+				},
+			},
 		},
-		// Gabe owes David $10
 		{
-			LenderUserID:   DavidID,
-			BorrowerUesrID: GabeID,
-			Share:          decimal.NewFromInt(10),
-		},
-		// Fred owes Bob $10
-		{
-			LenderUserID:   BobID,
-			BorrowerUesrID: FredID,
-			Share:          decimal.NewFromInt(10),
-		},
-		// Fred owes Charlie $30
-		{
-			LenderUserID:   CharlieID,
-			BorrowerUesrID: FredID,
-			Share:          decimal.NewFromInt(30),
-		},
-		// Fred owes David $10
-		{
-			LenderUserID:   DavidID,
-			BorrowerUesrID: FredID,
-			Share:          decimal.NewFromInt(10),
-		},
-		// Fred owes Ema $10
-		{
-			LenderUserID:   EmaID,
-			BorrowerUesrID: FredID,
-			Share:          decimal.NewFromInt(10),
-		},
-		// Bob owes Charlie $40
-		{
-			LenderUserID:   CharlieID,
-			BorrowerUesrID: BobID,
-			Share:          decimal.NewFromInt(40),
-		},
-		// Charlie owes David $20
-		{
-			LenderUserID:   DavidID,
-			BorrowerUesrID: CharlieID,
-			Share:          decimal.NewFromInt(20),
-		},
-		// David owes Ema $50
-		{
-			LenderUserID:   EmaID,
-			BorrowerUesrID: DavidID,
-			Share:          decimal.NewFromInt(50),
+			name: "valie 2",
+			ledgers: []*types.Ledger{
+				{
+					LenderUserID:   AliceID,
+					BorrowerUesrID: BobID,
+					Share:          decimal.NewFromFloat(92.0),
+				},
+				{
+					LenderUserID:   BobID,
+					BorrowerUesrID: AliceID,
+					Share:          decimal.NewFromFloat(1.24),
+				},
+				{
+					LenderUserID:   BobID,
+					BorrowerUesrID: AliceID,
+					Share:          decimal.NewFromFloat(7.61),
+				},
+				{
+					LenderUserID:   BobID,
+					BorrowerUesrID: AliceID,
+					Share:          decimal.NewFromFloat(41.4),
+				},
+				{
+					LenderUserID:   AliceID,
+					BorrowerUesrID: BobID,
+					Share:          decimal.NewFromFloat(5.1),
+				},
+				{
+					LenderUserID:   BobID,
+					BorrowerUesrID: AliceID,
+					Share:          decimal.NewFromFloat(73),
+				},
+				{
+					LenderUserID:   AliceID,
+					BorrowerUesrID: BobID,
+					Share:          decimal.NewFromFloat(7.22),
+				},
+			},
+			expectFail: false,
+			expectBalance: []*types.Balance{
+				{
+					SenderUserID:   BobID,
+					ReceiverUserID: AliceID,
+					Share:          decimal.NewFromFloat(18.93),
+				},
+			},
 		},
 	}
 
-	// sender: balance
-	expectBalance := []*types.Balance{
-		// Ema send Fred $60
-		{
-			SenderUserID:   EmaID,
-			ReceiverUserID: FredID,
-			Share:          decimal.NewFromInt(60),
-		},
-		// Charlie send Gabe $40
-		{
-			SenderUserID:   CharlieID,
-			ReceiverUserID: GabeID,
-			Share:          decimal.NewFromInt(40),
-		},
-		// Gabe send David $20
-		{
-			SenderUserID:   CharlieID,
-			ReceiverUserID: DavidID,
-			Share:          decimal.NewFromInt(10),
-		},
-	}
+	// first test case
+	t.Run(subtests[0].name, func(t *testing.T) {
+		balanceList := expense.DebtSimplify(subtests[0].ledgers)
 
-	t.Run("valid", func(t *testing.T) {
-		balanceList := expense.DebtSimplify(ledgers)
-
-		assert.Equal(t, len(expectBalance), len(balanceList))
+		assert.Equal(t, len(subtests[0].expectBalance), len(balanceList))
 
 		for _, b := range balanceList {
-			expectBalance := findBalance(expectBalance, b.SenderUserID, b.ReceiverUserID)
+			expectBalance := findBalance(subtests[0].expectBalance, b.SenderUserID, b.ReceiverUserID)
 
 			assert.NotNil(t, expectBalance)
 			assert.True(t, expectBalance.Share.Equal(b.Share))
@@ -112,13 +172,26 @@ func TestDebtSimplify(t *testing.T) {
 
 		// make sure Alice does not owe or be owed by anyone
 		var balance *types.Balance
-		for _, b := range expectBalance {
+		for _, b := range subtests[0].expectBalance {
 			if b.SenderUserID == AliceID || b.ReceiverUserID == AliceID {
 				balance = b
 				break
 			}
 		}
 		assert.Nil(t, balance)
+	})
+
+	// second test case
+	t.Run(subtests[1].name, func(t *testing.T) {
+		balanceList := expense.DebtSimplify(subtests[0].ledgers)
+
+		assert.Equal(t, len(subtests[0].expectBalance), len(balanceList))
+		for _, b := range balanceList {
+			expectBalance := findBalance(subtests[0].expectBalance, b.SenderUserID, b.ReceiverUserID)
+
+			assert.NotNil(t, expectBalance)
+			assert.True(t, expectBalance.Share.Equal(b.Share))
+		}
 	})
 }
 

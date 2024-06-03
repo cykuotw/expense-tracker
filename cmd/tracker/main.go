734a7ebc23c66378312a046856243fcaf3bf2518
cykuo <cykuotw45@gmail.com>
2024-06-02 21:59:13 -0400

    feat: add frontend service to main
    - api/frontend service run in parallel
    - can be shutdown gracefully

--- cmd/tracker/main.go
+++ cmd/tracker/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"context"
 	"expense-tracker/cmd/tracker/api"
+	"expense-tracker/cmd/tracker/frontend"
 	"expense-tracker/config"
 	"expense-tracker/db"
 	"log"
+	"net/http"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
@@ -19,8 +25,39 @@ func main() {
 		log.Fatal(err)
 	}
 
-	server := api.NewAPIServer("localhost:8080", db)
-	if err := server.Run(); err != nil {
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	apiServer := api.NewAPIServer("localhost:8080", db)
+	go func() {
+		if err := apiServer.Run(); err != nil && err != http.ErrServerClosed {
+			log.Fatal(err)
+		}
+	}()
+
+	frontendServer := frontend.NewFrontendServer("localhost:8085")
+	go func() {
+		if err := frontendServer.Run(); err != nil && err != http.ErrServerClosed {
+			log.Fatal(err)
+		}
+	}()
+
+	<-ctx.Done()
+
+	stop()
+	log.Println("Shutting down system")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+	if err := apiServer.Shutdown(ctx); err != nil {
 		log.Fatal(err)
 	}
+	log.Println("\tAPI Server is shut down")
+
+	ctx, cancel = context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+	if err := frontendServer.Shutdown(ctx); err != nil {
+		log.Fatal(err)
+	}
+	log.Println("\tFrontend Server is shut down")
 }

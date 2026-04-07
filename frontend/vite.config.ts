4c7a32239b381768a61dded8e95904c3bef4970b
cykuo <cykuotw45@gmail.com>
2026-04-07 00:00:54 -0400

    dep: add vitest pakcage

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,27 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
+    plugins: [tailwindcss(), ...(process.env.VITEST ? [] : [react()])],
+    build: {
+        rollupOptions: {
+            onwarn(warning, warn) {
+                if (
+                    warning.message.includes('"use client"') &&
+                    warning.id?.includes("react-hot-toast")
+                ) {
+                    return;
+                }
+
+                warn(warning);
+            },
+        },
+    },
+    test: {
+        environment: "jsdom",
+        setupFiles: "./src/test/setup.ts",
+        css: true,
+    },
 });
